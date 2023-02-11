4860b24ead0bf473c5c81f8098aa7b9246ce8c93
viju-dev <vijaydorugade20@gmail.com>
2023-02-11 23:33:45 +0530

    initial commit
    all check

--- src/main/java/com/driver/DeliveryPartner.java
+++ src/main/java/com/driver/DeliveryPartner.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class DeliveryPartner {
 
-    private String id;
+    private final String id;
     private int numberOfOrders;
 
     public DeliveryPartner(String id) {
@@ -14,7 +14,7 @@ public class DeliveryPartner {
         return id;
     }
 
-    public Integer getNumberOfOrders(){
+    public Integer getNumberOfOrders() {
         return numberOfOrders;
     }
 
